1f2c095a455abeaaeaeb9aee60629005543bbf6d
Trung Nguyen <trungnv184@gmail.com>
2020-10-19 16:07:47 +0700

    chore: config eslit and prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const a = [1, 2];
+  const x = {
+    a: 1,
+    b: 2,
+  };
   return (
     <div className="App">
       <header className="App-header">
