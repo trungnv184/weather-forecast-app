edb6cf241c03674990de1d50426a2fd4042092e9
Trung Nguyen <trungnv184@gmail.com>
2020-10-24 12:45:10 +0700

    feat: catch error with ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Weathers from "./containers/Weathers/Weathers";
 import Layout from "./hocs/Layout/Layout";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 const App = () => {
   return (
     <div className="App">
       <Layout>
-        <Weathers />
+        <ErrorBoundary>
+          <Weathers />
+        </ErrorBoundary>
       </Layout>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import {LocationProvider} from "./contexts/LocationContext";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
+import {stopReportingRuntimeErrors} from "react-error-overlay";
+
+if (process.env.NODE_ENV === "development") {
+  stopReportingRuntimeErrors(); // disables error overlays
+}
 
 const RootApp = () => {
   return (
